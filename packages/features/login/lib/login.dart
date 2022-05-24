0be7aead1406803ab147b912820523e247d024cd
Biplab Dutta <biplabdutta860@gmail.com>
2022-05-24 11:20:49 +0545

    add login package

---
+++ packages/features/login/lib/login.dart
@@ -0,0 +1,3 @@
+library login;
+
+export 'src/login.dart';
---
+++ packages/features/login/test/src/login_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:login/login.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Login', () {
+    test('can be instantiated', () {
+      expect(Login(), isNotNull);
+    });
+  });
+}
